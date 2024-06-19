d44169af4b5489bae85db745461d7588b3a73408
Skitttz <skittz.dev@gmail.com>
2024-06-19 15:45:32 -0300

    feat: data models

---
+++ src/data/models/remote-load-products.ts
@@ -0,0 +1,10 @@
+export type RemoteProduct = {
+  id: number;
+  name: string;
+  description: string;
+  material: string;
+  price: number;
+  sizes: string[];
+  colors: string[];
+  images: string[];
+}
